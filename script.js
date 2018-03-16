9c31bfd97b8a534b2308f6092d2e751a9a355abd
Ryan <jragard@gmail.com>
2018-03-16 07:59:45 -0400

    added randomly placed mines

--- script.js
+++ script.js
@@ -1,65 +1,33 @@
 let container_div = document.getElementById("container");
 let boardArray = [];
-let cellArray = [];
+let cellNum = 0;
+let mineCellIndex;
 
 function board(target_div) {
 
     this.generateCells = function (widthInput, heightInput, mineInput) {
 
-        let boardArray = [];
-        
-        for (let row = 0; row < widthInput; row++) {
-            
-            for (let col = 0; col < heightInput; col++) {
+        for (let i = 0; i < heightInput; i++) {
+            boardArray.push([]);
+            for (let j = 0; j < widthInput; j++) {
+                cellNum++;
+                boardArray[i].push(cellNum);
                 this.cell = document.createElement("div");
                 this.cell.className = "cell";
-                this.cell.style.backgroundColor = "gray";
+                this.cell.id = cellNum;
                 this.cell.addEventListener("contextmenu", this.rightClick, false);
                 this.cell.addEventListener("click", this.leftClick);
                 target_div.appendChild(this.cell);
+                
             }
         }
-            // let cellArray = [];
-            // let boardArray = [];
-            // let mineArray = [];
-            
-            // for (let mine = 0; mine < mineInput; mine++) {
-            //     mineArray.push("mines" + mine)
-            // }
-
-            // for (let i = 0; i < (widthInput * heightInput); i++) {
-            //     cellArray
-            // }
-
-            // for (let i = 0; i < (widthInput * heightInput); i++) {
-            //     boardArray.push("cell");
-            // }
-
-            // for (let mine = 0; mine <= mineInput; mine++) {
-            //     boardArray.splice(Math.floor(Math.random() * (widthInput * heightInput)), 1, "mines");
-            // }     
-
-            // for (let row = 0; row < boardArray.length; row++) {
-            //     let cellArray = document.getElementsByClassName("cell");
-            // }
-
-                
-            
-            
-            
-            
-            
-            
-                // now cellsArray is an array of ["cells", "cells", "cells", "cells", "cells", "cells", "cells", "cells", "cells",]
-                // maybe i can compare cellsArray with boardArray?  (which contains the correct classes I want for each of the cells.)  Or do I 
-                // even need cellsArray in the first place?  One thing is certain, I will need a nested array (one array for each row), so that I can map out cell
-                // positions to figure out where bombs are, etc.
-
-                // Also I may need to move these functions below around a little bit 
-
-            
-
         
+        for (let mine = 0; mine < mineInput; mine++) {
+            let mineCellIndex = Math.floor(Math.random() * (widthInput * heightInput));
+            let cellToReplace = document.getElementById(mineCellIndex);
+
+            cellToReplace.className = "mines";
+        }
     }
 
     
@@ -77,36 +45,25 @@ function board(target_div) {
     this.rightClick = function (event) {
         let target = event.target;
         event.preventDefault();
-        if (target.style.backgroundColor === "red") {
-        target.style.backgroundColor = "gray";
-       } else if (target.style.backgroundColor === "gray") {
-            target.style.backgroundColor = "red";
+        if (target.className === "cell") {
+            target.className = "flag";
+        
+       } else if (target.className === "flag") {
+            target.className = "cell";
        }
         return false;
+        console.log(target.style.backgroundColor);
     }
 
     this.leftClick = function (event) {
         let target = event.target;
         console.log(target.className);
         target.className = "safeCells";
-        target.style.backgroundColor = "white";
-
+        
         let widthInput = document.getElementById("width").value;
         let heightInput = document.getElementById("height").value;
         let mineInput = Number(document.getElementById("mines").value);
-        
-        for (let i = 0; i < (widthInput * heightInput); i++) {
-            boardArray.push("cell");
-        }
-
-        for (let mine = 0; mine <= mineInput; mine++) {
-            boardArray.splice(Math.floor(Math.random() * (widthInput * heightInput)), 1, "mines");
-        }     
-
-        for (let row = 0; row < boardArray.length; row++) {
-            let cellArray = document.getElementsByClassName("cell");
-        }
-    }
+     }
 
     let button = document.getElementById("startGame");
     button.addEventListener("click", this);
@@ -116,4 +73,3 @@ function board(target_div) {
 var object = new board(container_div);
 
 
-// Math.random() * mineInput
